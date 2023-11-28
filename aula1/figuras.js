f4e9b15baf64debc837733d1d899378b578090cf
Bruno Leonardo Monteiro <bruno.lmonteiro@totvs.com.br>
2023-11-27 22:13:55 -0300

    Figuras geométricas

---
+++ aula1/figuras.js
@@ -0,0 +1,27 @@
+// Protótipo
+class Quadrado {
+    base
+    altura
+    cor
+    constructor(base, altura, cor) {
+      this.base = base
+      this.altura = altura
+      this.cor = cor || 'green'
+    }
+    calcularArea() {
+      return this.base * this.altura
+    }
+    imprimir() {
+      return `<div style='width:${this.base}px; height:${this.altura}px;
+              background-color:${this.cor}'></div>`
+    }
+  }
+  
+  //Produção
+  const q1 = new Quadrado(100, 100, 'red')
+  const q2 = new Quadrado(500, 500)
+  
+  console.log(q1.calcularArea())
+  console.log(q2.cor)
+
+  document.write(q2.imprimir())
