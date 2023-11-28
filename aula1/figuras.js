bbc1194a4265bb705c62a51825651b7f8a7d0d1f
Bruno Leonardo Monteiro <bruno.lmonteiro@totvs.com.br>
2023-11-27 22:15:05 -0300

    Ajuste na formatação

--- aula1/figuras.js
+++ aula1/figuras.js
@@ -1,27 +1,27 @@
 // Protótipo
 class Quadrado {
-    base
-    altura
-    cor
-    constructor(base, altura, cor) {
-      this.base = base
-      this.altura = altura
-      this.cor = cor || 'green'
-    }
-    calcularArea() {
-      return this.base * this.altura
-    }
-    imprimir() {
-      return `<div style='width:${this.base}px; height:${this.altura}px;
-              background-color:${this.cor}'></div>`
-    }
+  base
+  altura
+  cor
+  constructor(base, altura, cor) {
+    this.base = base
+    this.altura = altura
+    this.cor = cor || 'green'
   }
-  
-  //Produção
-  const q1 = new Quadrado(100, 100, 'red')
-  const q2 = new Quadrado(500, 500)
-  
-  console.log(q1.calcularArea())
-  console.log(q2.cor)
+  calcularArea() {
+    return this.base * this.altura
+  }
+  imprimir() {
+    return `<div style='width:${this.base}px; height:${this.altura}px;
+              background-color:${this.cor}'></div>`
+  }
+}
 
-  document.write(q2.imprimir())
+//Produção
+const q1 = new Quadrado(100, 100, 'red')
+const q2 = new Quadrado(500, 500)
+
+console.log(q1.calcularArea())
+console.log(q2.cor)
+
+document.write(q2.imprimir())
